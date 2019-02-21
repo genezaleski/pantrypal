c0ec9a84f4a5e9b48573487112e367e9f84f4db6
David Liotta <liottad6@students.rowan.edu>
2019-02-21 14:22:33 -0500

    Basic home page

---
+++ home.php
@@ -0,0 +1,4 @@
+<title>Pantry Pal</title>
+<h1> Pantry Pal </h1><br>
+<input type="text" name="searchBar" placeholder="put ingredients or recipe name here">
+<button type="submit" name="search">Search</button>
