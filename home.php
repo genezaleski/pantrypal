a766d50adc3f1946d31d51ba7a49e0d304f83942
David Liotta <liottad6@students.rowan.edu>
2019-02-26 14:42:30 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,8 @@
 <title>Pantry Pal</title>
+<?php
+include 'navbar.php';
+
+?>
 <h1> Pantry Pal </h1><br>
 <input type="text" name="searchBar" placeholder="put ingredients or recipe name here">
 <button type="submit" name="search">Search</button>
