953a51c4367be8177eca2ed1daa2a49652513d9f
Gene Zaleski <zaleskig8@students.rowan.edu>
2019-03-05 15:51:06 -0500

    centered search bar

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <style>
 body{
-	background-image: url(images/carrots-food-fresh-616404.jpg);
-	background-size: 100%;
+    background-image: url(images/carrots-food-fresh-616404.jpg);
+    background-size: 100%;
+}
+
+.searchbar{
+    margin-left: 675px;
+    margin-top: 300px;
+    width: 300px;
+}
+
+.searchtitle{
+    margin-left: 675px;
+    margin-top: 300px;
+    margin-bottom: -300px;
+    padding: 0px;
 }
 </style>
 
@@ -10,6 +23,7 @@ body{
 include 'navbar.php';
 
 ?>
-<h1> Pantry Pal </h1><br>
-<input type="text" name="searchBar" placeholder="put ingredients or recipe name here">
+<h1 class="searchtitle"> Pantry Pal </h1><br>
+<input class="searchbar" type="text" name="searchBar" placeholder="put ingredients or recipe name here">
 <button type="submit" name="search">Search</button>
+
