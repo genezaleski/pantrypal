cb9033b7cd2c274641dfa320210ebb4330f31433
Gene Zaleski <zaleskig8@students.rowan.edu>
2019-03-08 18:08:59 -0500

    implemented search by recipe name and search by ingredients

--- reciperesults.php
+++ reciperesults.php
@@ -15,19 +15,36 @@ body{
 </style>
 
 <title>Search Results:</title>
+
 <?php
 include 'navbar.php';
 
-$api_key = "'X-RapidAPI-Key : a44d550177msh8aeb1867319b60bp1fbbc5jsn1d9edc60417a'";
-$api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=10";
+$search = $_POST['searchBar'];
 
-$cmd = "curl -H " . $api_key . " " . $api_url;
+if(!empty($search)){
+    $api_key = '"X-RapidAPI-Key : a44d550177msh8aeb1867319b60bp1fbbc5jsn1d9edc60417a"';
+    $api_url = null;
+    if(strpos($search, ',') !== false ) { //If commas are in search string, search by ingredients
+        $api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" . urlencode($search);
+    }
+    else{                                 //If no commas, search by recipe name
+        $api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=" . $search;
+    }
+    
 
-$output = shell_exec($cmd);
+    $cmd = "curl -H " . $api_key . " " . $api_url;
 
-echo $output;
+    $output_arr = json_decode(shell_exec($cmd),true);
+    
 
-?>
+
+   for($i = 0; $i < 10; $i++){
+        print_r($output_arr['results'][$i]['title']."\n");
+        echo "<br>";
+   }
+
+}
+?> <!--End PHP-->
 
 <form name="recipeSearch" method="post" action="reciperesults.php">
     <input class="searchbar" type="text" name="searchBar" placeholder="put ingredients or recipe name here">
