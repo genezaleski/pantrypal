9657d5b1e4cd1428590c07e5bec0b596755e157a
David Liotta <liottad6@students.rowan.edu>
2019-03-12 15:32:22 -0400

    apikey change

--- recipieInfo.php
+++ recipieInfo.php
@@ -4,8 +4,8 @@ include 'navbar.php';
 $recipieID = $_GET['id'];
 
 echo $recipieID;
-$api_key = "322dc0a550msh6970a9bebfd18b2p1010fcjsnaed4930a9684";
-$api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/$recipieID/information";
+$api_key = '"X-RapidAPI-Key :322dc0a550msh6970a9bebfd18b2p1010fcjsnaed4930a9684"';
+$api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/".$recipieID."/information";
 $cmd = "curl -H " . $api_key . " " . $api_url;
 $output_arr = json_decode(shell_exec($cmd),true);
 
@@ -16,5 +16,6 @@ $output_arr = json_decode(shell_exec($cmd),true);
 //);
 
 
-echo $output_arr["sourceUrl"];
+echo $output_arr['sourceUrl'];
+echo $output_arr['cookingMinutes'];
 ?>
