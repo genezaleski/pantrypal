91b41c3fd202b7a844bf16fee4932c9c5648f2c1
Nick <hutchinsn7@students.rowan.edu>
2019-04-01 18:45:17 -0400

    Testing read_one endpoint for Recipe

--- api/Recipe/read_one.php
+++ api/Recipe/read_one.php
@@ -18,7 +18,13 @@ $db = $database->getConnection();
 $Recipe = new Recipe($db);
 
 // set ID property of recipe to read
-$Recipe->recipe_id = curl_unescape($recipe_id, "recipe_id");
+$url = '/api/Recipe/read_one/';
+$ch = curl_init($url);
+$readString = http_build_query($data, '', '&');
+$response = curl_exec($ch);
+curl_close($ch);
+
+$Recipe->recipe_id = curl_unescape($respone, $recipe_id);
 
 // read the details of recipe to be edited
 $Recipe->readOne();
