44d7df20509999e99a392369a1ece8e9b7a2a7a8
Nick <hutchinsn7@students.rowan.edu>
2019-04-01 12:44:27 -0400

    Testing readOne() Recipe endpoint

--- api/PantryItem/read.php
+++ api/PantryItem/read.php
@@ -21,17 +21,11 @@ $num = $stmt->rowCount();
 // check if more than 0 record found
 if($num>0){
 
-    // ratings array
+    // pantry array
     $PantryItem_arr=array();
     $PantryItem_arr["PantryItem"]=array();
 
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackover:flow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
         extract($row);
 
         $Pantry_item=array(
@@ -40,7 +34,7 @@ if($num>0){
             "user_id" => $user_id
         );
 
-	array_push($PantryItem_arr["PantryItem"], $PantryItem_item);
+	array_push($PantryItem_arr["PantryItem"], $Pantry_item);
     }
 
     // set response code - 200 OK
@@ -54,7 +48,7 @@ if($num>0){
     // set response code - 404 Not found
     http_response_code(404);
 
-    // tell the user no ratings found
+    // tell the user no pantry items found
     echo json_encode(
         array("message" => "No pantry items found.")
     );
