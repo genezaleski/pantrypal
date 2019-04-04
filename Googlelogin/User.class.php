be5849d93fc89d909143c3e70f468551452a26b6
Eric1215 <38734122+Eric1215@users.noreply.github.com>
2019-04-04 15:04:06 -0400

    Add files via upload

--- Googlelogin/User.class.php
+++ Googlelogin/User.class.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class User {
 	private $dbHost     = DB_HOST;
     private $dbUsername = DB_USERNAME;
@@ -23,15 +22,22 @@ class User {
 	function checkUser($userData = array()){
         if(!empty($userData)){
             // Check whether user data already exists in the database
-            $checkQuery = "SELECT * FROM ".$this->userTbl." WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'";
+            $checkQuery = "SELECT * FROM ".$this->userTbl." WHERE oauth_uid = '".$userData['oauth_uid']."'";
             $checkResult = $this->db->query($checkQuery);
             if($checkResult->num_rows > 0){
                 // Update user data if already exists
-                $query = "UPDATE ".$this->userTbl." SET first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', modified = NOW() WHERE oauth_provider = '".$userData['oauth_provider']."' AND oauth_uid = '".$userData['oauth_uid']."'";
+                $query = "UPDATE ".$this->userTbl." SET first_name = '".$userData['first_name']."', 
+                last_name = '".$userData['last_name']."', 
+                picture = '".$userData['picture']."', 
+                modified = NOW() WHERE oauth_uid = '".$userData['oauth_uid']."'";
                 $update = $this->db->query($query);
             }else{
                 // Insert user data in the database
-                $query = "INSERT INTO ".$this->userTbl." SET oauth_provider = '".$userData['oauth_provider']."', oauth_uid = '".$userData['oauth_uid']."', first_name = '".$userData['first_name']."', last_name = '".$userData['last_name']."', email = '".$userData['email']."', gender = '".$userData['gender']."', locale = '".$userData['locale']."', picture = '".$userData['picture']."', link = '".$userData['link']."', created = NOW(), modified = NOW()";
+                $query = "INSERT INTO ".$this->userTbl." SET oauth_uid = '".$userData['oauth_uid']."', 
+                first_name = '".$userData['first_name']."', 
+                last_name = '".$userData['last_name']."',  
+                picture = '".$userData['picture']."',
+                modified = NOW()";
                 $insert = $this->db->query($query);
             }
             
