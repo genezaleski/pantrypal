ee3f98207f6769ce849e11018af2189a1cc2cac3
Nick <hutchinsn7@students.rowan.edu>
2019-04-08 14:39:22 -0400

    Wrong file commited

--- api/config/database.php
+++ api/config/database.php
@@ -3,8 +3,8 @@ class Database{
     // specify your own database credentials
     private $host = "localhost";
     private $db_name = "seniorproject";
-    private $username = "root";
-    private $password = "password";
+    private $username = "seniorproject";
+    private $password = "diVed(22)Berth";
     public $conn;
 
     // get the database connection
@@ -13,7 +13,7 @@ class Database{
         $this->conn = null;
 
         try{
-            $this->conn = new PDO("mysql: host=" . $this->host . ";dbname=" . $this->db_name,
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name,
               $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
@@ -24,3 +24,17 @@ class Database{
     }
 }
 ?>
+
+
+
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
