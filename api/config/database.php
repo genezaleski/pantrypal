6711b4b692401349ebea94cd15e530e45c4f1fd6
William H. Jacobs <billjacobs9395@yahoo.com>
2019-04-15 10:30:40 -0400

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -24,17 +24,3 @@ class Database{
     }
 }
 ?>
-
-
-
-$host="localhost";
-$port=3306;
-$socket="";
-$user="root";
-$password="";
-$dbname="";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
