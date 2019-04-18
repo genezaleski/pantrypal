bdd1369e97458d4a228e6787353d8ee920501b31
David Liotta <liottad6@students.rowan.edu>
2019-04-18 13:57:22 -0400

    Fixed logic in recipe creation

--- recipeInfo.php
+++ recipeInfo.php
@@ -5,23 +5,34 @@ include 'navbar.php';
 $recipeID = $_GET['id'];
 $my_api_key = '"X-RapidAPI-Key :322dc0a550msh6970a9bebfd18b2p1010fcjsnaed4930a9684"';
 $other_api_key = '"X-RapidAPI-Key : 4af690163bmshda5b867e43cbc70p155394jsnc38cedc3355a"';
+
 //Retrieving recipe info
 $api_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" . $recipeID . "/information";
 $cmd = "curl -H " . $my_api_key . " " . $api_url;
 $recipeInfo = json_decode(shell_exec($cmd), true);
+
 //Retriving recipe instructions broken into steps
 //$analysed_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/".$recipeID."/analyzedInstructions?stepBreakdown=false";
 //$instrCmd = "curl -H " . $api_key . " " . $analysedRecipe;
 //$recipeInstr = json_decode(shell_exec($instrCmd),true);
+
 //Retriving similar recipies to add as links
 $related_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" . $recipeID . "/similar";
 $relatedCmd = "curl -H " . $other_api_key . " " . $related_url;
 $relatedLinks = json_decode(shell_exec($relatedCmd), true);
-//Pulling in likes and dislikes  to be used 
+
+//Pulling in likes and dislikes to be used 
 $likeDataCmd = "curl http://52.91.254.222/api/RateRecipe/read.php";
 $decodedLikeData = json_decode(shell_exec($likeDataCmd), true);
+
+//Pulling in comments
 $commentCmd = "curl http://52.91.254.222/api/CommentRecipe/read.php";
 $decodedComments = json_decode(shell_exec($commentCmd), true);
+
+//Pulling in recipes to check if the current one exists
+$recipeCmd = "curl http://52.91.254.222/api/Recipe/read.php";
+$rListDecode = json_decode(shell_exec($recipeCmd), true);
+
 $rExists = false;
 for ($r = 0; $r < sizeof($rListDecode['Recipes']); $r++) {
     if ($rListDecode['Recipes'][$r]['api_recipe_id'] == $recipeID) {
@@ -90,6 +101,7 @@ if($rExists == false){
             ?>
 
             <script language="javascript">
+
                 function changeLikeImage() {
                     if (document.getElementById("likeBtn").src.includes('likeButton.png')) {
                         //Checking if the disLiked button is checked and unchecking it
@@ -97,9 +109,8 @@ if($rExists == false){
                             document.getElementById("disLikeBtn").src = "images/likeButton.png";
                         }
                         document.getElementById("likeBtn").src = "images/likedButton.png";
-                        //Set recipe to liked here using MySQL
-                        //curl - d '{"recipe_id" : "'.$recipeID.
-                        //'", "user_id" : "9", "rating" : "like"}' - H "Content-Type: application/json" - X POST 'http://localhost:80/api/RateRecipe/create.php';
+                        //Set recipe to liked here
+                        sendLike();
                     } else {
                         document.getElementById("likeBtn").src = "images/likeButton.png";
                         //Remove like value from database
@@ -112,13 +123,17 @@ if($rExists == false){
                             document.getElementById("likeBtn").src = "images/likeButton.png";
                         }
                         document.getElementById("disLikeBtn").src = "images/disLikedButton.png";
-                        //Set recipe to disliked here using MySQL
-                        //curl - d '{"recipe_id" : "'.$recipeID.
-                        //'", "user_id" : "9", "rating" : "dislike"}' - H "Content-Type: application/json" - X POST 'http://localhost:80/api/RateRecipe/create.php';
+                        //Set recipe to disliked here
                     } else {
                         document.getElementById("disLikeBtn").src = "images/likeButton.png";
                         //Remove disLike from database
                     }
+                }
+                function sendLike(){
+                    var xmlhtml = new XMLHttpRequest();
+                    xmlhtml.open('POST','ajax_scripts/sendLike.php',true);
+                    xmlhtml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                    xmlhtml.send("user="+3+"rID="+<?php echo $recipeID ?>);
                 }
                 function postComment(phpComment){
                     var xmlhtml = new XMLHttpRequest();
@@ -161,24 +176,6 @@ if($rExists == false){
 
             <?php
             $url_post = 'http://52.91.254.222/api/CommentRecipe/create.php';
-            /*
-            $newComment = json_encode(array(
-                'user_id' => 4,
-                'recipe_id' => 2,
-                'comment_text' => $_POST['comment']
-            ));
-            $options = array(
-                'http' => array(
-                    'method'  => 'POST',
-                    'content' => $newComment,
-                    'header' =>  "Content-Type: application/json\r\n" .
-                        "Accept: application/json\r\n"
-                )
-            );
-            $stream  = stream_context_create($options);
-            $result = file_get_contents($url_post, false, $stream);
-            $response = json_decode($result);
-            */
 
             //Loop that prints out all comments for current recipe
             for ($i = 0; $i < sizeOf($decodedComments['comments']); $i++) {
