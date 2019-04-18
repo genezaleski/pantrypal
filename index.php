e98fdb0f4a4690df137479400fff7743a890ebb9
David Smits <smitsd0@students.rowan.edu>
2019-04-18 14:53:31 -0400

    Added random vegan recipes to the index page

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="stylesheets/resultspagestyle.css">
 <head>
 <meta   name="google-site-verification"
         content="W0LpZ8p6M13ICggYTyUZqikpTXnPCnY-0d_U6uQC5p0" />
@@ -104,7 +105,75 @@ include 'navbar.php';
 
 <div id="Vegan" class="tabcontent">
   <h3>Vegan</h3>
-  <p>Vegan Recipes Go Here.</p>
+  <?php
+
+  $isveggie ='';
+  if(isset($_POST['Vegetarian'])){
+      $isveggie = $isveggie . "diet=vegetarian&";
+  }
+
+  $image_results = array();
+  $id = null;
+  $id_list = array();
+  $title = null;
+  $title_list = array();
+
+  $api_key = "'X-RapidAPI-Key : d466494462msh9686e88c15be8cfp108f2ejsnbc67ad6ec517'";
+  $api_url = "'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=12&tags=vegetarian%2Cvegan'";
+  $api_host = "'X-RapidAPI-Host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'";
+  $cmd = "curl " . $api_url . "  -H " . $api_key;
+  $output_arr = json_decode(shell_exec($cmd),true);
+  $results = $output_arr['recipes'];
+  $output_arr = $results;
+
+  for($i = 0; $i < sizeof($output_arr); $i++){
+      $image = $output_arr[$i]['image'];
+      $id = $output_arr[$i]['id'];
+      $title = $output_arr[$i]['title'];
+
+      array_push($id_list,$id);
+      array_push($title_list,$title);
+      array_push($image_results,$image);
+  }
+
+  $one = '';
+  $two = '';
+  $three = '';
+
+  //Puts images into columns
+  for($i = 0; $i < sizeof($image_results); $i++){
+      if(($i % 3) == 1){
+          $one .= '<div class="imglink">
+                   <a href=recipeInfo.php?id='.$id_list[$i].'><img class="imgresults" src="'. $image_results[$i] .'" alt="recipeImage" style="width:100%;"></a>
+                   <div class="recipeName">' .$title_list[$i]. '</div>
+                   </div>';
+      }
+      else if(($i % 3) == 2){
+          $two .= '<div class="imglink">
+                   <a href=recipeInfo.php?id='.$id_list[$i].'><img class="imgresults" src="'. $image_results[$i] .'" alt="recipeImage" style="width:100%;"></a>
+                   <div class="recipeName">' .$title_list[$i]. '</div>
+                   </div>';
+      }
+      else if(($i % 3) == 0){
+          $three .= '<div class="imglink">
+                     <a href=recipeInfo.php?id='.$id_list[$i].'><img class="imgresults" src="'. $image_results[$i] .'" alt="recipeImage" style="width:100%;"></a>
+                     <div class="recipeName">' .$title_list[$i]. '</div>
+                     </div>';
+      }
+  }
+
+  echo '<div class="row">
+          <div class="column">
+          ' . $one . '
+          </div>
+          <div class="column">
+          ' . $two . '
+          </div>
+          <div class="column">
+          ' . $three . '
+          </div>
+      </div>';
+  ?>
 </div>
 
 <script>
