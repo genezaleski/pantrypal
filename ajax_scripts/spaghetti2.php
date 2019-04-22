e621edc790f867e45fdf9eb25629021c51535630
bautaj7 <45546609+bautaj7@users.noreply.github.com>
2019-04-22 19:08:56 -0400

    3 functions to fill the session variables

---
+++ ajax_scripts/spaghetti2.php
@@ -0,0 +1,14 @@
+<?php
+    //uses spaghetti code to store session variables
+
+    session_start();
+
+    //removes existing fields
+    unset($_SESSION['name']);
+    unset($_SESSION['image']);
+
+    //fills session variable with useful profile information
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['image'] = $_POST['imageUrl'];
+
+    ?>
---
+++ ajax_scripts/spaghetti3.php
@@ -0,0 +1,15 @@
+<?php
+    //uses spaghetti code to store session variables
+
+    session_start();
+
+    //removes existing fields
+    unset($_SESSION['firstName']);
+    unset($_SESSION['lastName']);
+
+    //fills session variable with useful profile information
+    $_SESSION['firstName'] = $_POST['firstName'];
+    $_SESSION['lastName'] = $_POST['lastName'];
+
+
+    ?>
