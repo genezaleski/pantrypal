8a0f6f26cfe72b866e44a034c29efa2dd8f2947b
Eric1215 <38734122+Eric1215@users.noreply.github.com>
2019-04-23 18:38:53 -0400

    Update Google_Auth.php

--- Googlelogin/Google_Auth.php
+++ Googlelogin/Google_Auth.php
@@ -1,28 +1,10 @@
 <?php
-/*
- * Copyright 2010 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 require_once "Google_AuthNone.php";
 require_once "Google_OAuth2.php";
 
-/**
- * Abstract class for the Authentication in the API client
- * @author Chris Chabot <chabotc@google.com>
- *
- */
+
 abstract class Google_Auth {
   abstract public function authenticate($service);
   abstract public function sign(Google_HttpRequest $request);
