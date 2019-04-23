ebbbcf224f17b51d96d63ab5e677a20d6063eb99
Gene Zaleski <zaleskig8@students.rowan.edu>
2019-04-22 23:40:28 -0400

    fixed user id posting to

--- ajax_scripts/spaghetti4.php
+++ ajax_scripts/spaghetti4.php
@@ -2,8 +2,9 @@
   session_start();
 
   $user = $_POST['email'];
-  $url = "'52.91.254.222/api/User/read_one.php'";
-  $cmd = "curl " . $url . "?user_name=" . $user;
+  //$url = "'52.91.254.222/api/User/read_one.php'";
+  //$cmd = "curl " . $url . "?user_name=" . $user;
+  $cmd = 'curl "http://52.91.254.222/api/User/read_one.php?user_name='.$user.'"';
   //echo $cmd;
   $output_arr = json_decode(shell_exec($cmd),true);
   $id = $output_arr['user_id'];
