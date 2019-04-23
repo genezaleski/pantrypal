8d980405b7ec70fba938f93dba1566944446adcf
Eric1215 <38734122+Eric1215@users.noreply.github.com>
2019-04-23 18:43:14 -0400

    Update Google_P12Signer.php

--- Googlelogin/Google_P12Signer.php
+++ Googlelogin/Google_P12Signer.php
@@ -1,27 +1,6 @@
 <?php
-/*
- * Copyright 2011 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * Signs data.
- *
- * Only used for testing.
- *
- * @author Brian Eaton <beaton@google.com>
- */
+
 class Google_P12Signer extends Google_Signer {
   // OpenSSL private key resource
   private $privateKey;
