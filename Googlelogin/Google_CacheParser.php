a1f459589dd026c12ae027d427c5f06ddda44330
Eric1215 <38734122+Eric1215@users.noreply.github.com>
2019-04-23 18:41:04 -0400

    Update Google_CacheParser.php

--- Googlelogin/Google_CacheParser.php
+++ Googlelogin/Google_CacheParser.php
@@ -1,24 +1,5 @@
 <?php
-/*
- * Copyright 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Implement the caching directives specified in rfc2616. This
- * implementation is guided by the guidance offered in rfc2616-sec13.
- * @author Chirag Shah <chirags@google.com>
- */
+
 class Google_CacheParser {
   public static $CACHEABLE_HTTP_METHODS = array('GET', 'HEAD');
   public static $CACHEABLE_STATUS_CODES = array('200', '203', '300', '301');
