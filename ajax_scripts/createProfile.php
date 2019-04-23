cb20217850a25c63504d6240c33a939ac703e9af
Gene Zaleski <zaleskig8@students.rowan.edu>
2019-04-22 23:26:51 -0400

    attempt at createProfile.php

--- ajax_scripts/createProfile.php
+++ ajax_scripts/createProfile.php
@@ -31,7 +31,7 @@ function createProfile(){
             $url_post = "http://52.91.254.222/api/User/create.php";
             $createUser = json_encode(array(
                 'user_name' => $_POST['user'],
-                'oauth_token' => $_POST['id'],
+                'oauth_token' => 'aksdjlfasdfiuhkasjdf',//$_POST['id'],
                 'first_name' => $_POST['firstName'],
                 'last_name' => $_POST['lastName'],
                 'picture_path' => $_POST['imageurl']
@@ -44,24 +44,15 @@ function createProfile(){
                     'header' => "Content-Type: application/json\r\n"."Accept: application/json\r\n"
                 )
             );
-            $stream = stream_context_create($options);
-            $result = file_get_contents($url_post,false,$stream);
-            $response = json_decode($result);
-}//end createProfile
-/*
-//checks to see if username already exists in the database
-function searchByName(){
-    $searchUser = $_POST['user'];
-    $request = "http://52.91.254.222/api/User/read_one.php?user_name=" . $searchUser;
-    $cmd = "curl --location --request GET " . $request;
-    $output_arr = json_decode(shell_exec($cmd),true);
-    //this will be returned if the user exists
-    $result = $output_arr['user_name'];
-
-    if ($result !== $searchUser){
-        //adds the user to the database
-        createProfile();
-    }//
-
-}//end searchByName*/
+            //$stream = stream_context_create($options);
+            //$result = file_get_contents($url_post,false,$stream);
+            //$response = json_decode($result);
+                        //$url_post = 'http://52.91.254.222/api/CommentRecipe/create.php';
+            $ch = curl_init($url_post);
+            curl_setopt($ch,CURLOPT_POST,1);
+            curl_setopt($ch,CURLOPT_POSTFIELDS,$createUser);
+            curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+            $curl_result = curl_exec($ch);
+}
 ?>
+
