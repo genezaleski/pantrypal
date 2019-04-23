de92fbd6dc76bce1eb63ca1912870399d0d7eee2
bautaj7 <45546609+bautaj7@users.noreply.github.com>
2019-04-22 21:09:14 -0400

    Add files via upload

---
+++ ajax_scripts/spaghetti4.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+
+  $user = $_POST['email'];
+  $url = "'52.91.254.222/api/User/read_one.php'";
+  $cmd = "curl " . $url . "?user_name=" . $user;
+  //echo $cmd;
+  $output_arr = json_decode(shell_exec($cmd),true);
+  $id = $output_arr['user_id'];
+  //echo $id;
+  $_SESSION['user_id'] = $id;
+  
+  
+  ?>
