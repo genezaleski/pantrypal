c6b665e3a266eee76384e817d7fccd8d61c5c642
David Liotta <liottad6@students.rowan.edu>
2019-04-22 23:08:46 -0400

    Update devs.php

--- devs.php
+++ devs.php
@@ -8,7 +8,7 @@ echo "<p class='stuff'> Jared Bauta </p>";
 echo "<p class='stuff'> Eric DeAngelis </p>";
 echo "<p class='stuff'> Nick Hutchinson </p>";
 echo "<p class='stuff'> Bill Jacobs </p>";
-echo "<p class='stuff'> Dave Liotta </p>";
+echo "<p class='stuff'> David Liotta </p>";
 echo "<p class='stuff'> Dave Smits </p>";
 echo "<p class='stuff'> Gene Zaleski </p>";
 
